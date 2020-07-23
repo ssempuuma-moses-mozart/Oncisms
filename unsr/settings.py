1cf4f1c3390e048d598dc902d3441a97bda4b466
Ezati Joseph <josephezati@gmail.com>
2020-07-23 18:37:16 +0300

    Change debug

--- unsr/settings.py
+++ unsr/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'c+sr%xq#=(ix^4^pny=n(%mo(sqhpz(#0cj_xl31ww#3460#!k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['www.unser.co.ug','127.0.0.1']
 
